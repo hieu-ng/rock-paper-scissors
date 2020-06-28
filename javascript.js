e8647c9642d0289bf2fe961dc4725aaa2dd3b109
hnguye <hieutnguyen06@gmail.com>
2020-06-28 13:30:23 +1000

    Update app to play

--- javascript.js
+++ javascript.js
@@ -1,134 +1,119 @@
-let playerScore = 0;
-let computerScore = 0;
-let roundResult;
-let roundNumber = 0;
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+  player: 0,
+  computer: 0
+};
 
-// Function that creates the Computers choice at random
-function computerPlay() {
-  let result = Math.random();
+// Play game
+function play(e) {
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
+}
 
-  if (result <= 0.33) {
-    alert("Computer selected Rock");
-  } else if (result <= 0.66) {
-    alert("Computer selected Paper");
+// Get computers choice
+function getComputerChoice() {
+  const rand = Math.random();
+  if (rand < 0.34) {
+    return 'rock';
+  } else if (rand <= 0.67) {
+    return 'paper';
   } else {
-    alert("Computer selected Scissors");
+    return 'scissors';
   }
 }
 
-// Function that runs computerPlay() and then creates a players choice and compares the two
-function playRound(playerSelection, computerSelection) {
-  let choice = prompt("Round " + roundNumber + "\n\nRock, Paper, or Scissors?");
-  playerSelection =
-    choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-  computerSelection = computerPlay();
-
-  console.log(
-    "Player chose: " +
-      playerSelection +
-      " | " +
-      "Computer chose: " +
-      computerSelection
-  );
-
-  // Error & Draw check
-  if (
-    playerSelection !== "Rock" &&
-    playerSelection !== "Paper" &&
-    playerSelection !== "Scissors"
-  ) {
-    alert(
-      "Looks like the player made an invalid choice, awaiting a new choice..."
-    );
-    prompt('Please enter a valid choice. ("Rock", "Paper", or "Scissors")');
-    return playRound();
-  } else if (playerSelection === computerSelection) {
-    roundResult = "Draw!";
-    return;
-  }
-
-  // Player selects Rock
-  if (playerSelection == "Rock") {
-    if (computerSelection == "Paper") {
-      computerScore++;
-      roundResult = "You lose! Rock gets covered by Paper!";
-      return;
+// Get game winner
+function getWinner(p, c) {
+  if (p === c) {
+    return 'draw';
+  } else if (p === 'rock') {
+    if (c === 'paper') {
+      return 'computer';
     } else {
-      playerScore++;
-      roundResult = "You win! Rock smashes Scissors!";
-      return;
+      return 'player';
     }
-  }
-
-  // Player selects Paper
-  if (playerSelection == "Paper") {
-    if (computerSelection == "Scissors") {
-      computerScore++;
-      roundResult = "You lose! Paper gets cut by Scissors!";
-      return;
+  } else if (p === 'paper') {
+    if (c === 'scissors') {
+      return 'computer';
     } else {
-      playerScore++;
-      roundResult = "You win! Paper covers Rock!";
-      return;
+      return 'player';
     }
-  }
-
-  // Player selects Scissors
-  if (playerSelection == "Scissors") {
-    if (computerSelection == "Rock") {
-      computerScore++;
-      roundResult = "You lose! Scissors get smashed by Rock!";
-      return;
+  } else if (p === 'scissors') {
+    if (c === 'rock') {
+      return 'computer';
     } else {
-      playerScore++;
-      roundResult = "You win! Scissors cut Paper!";
-      return;
+      return 'player';
     }
   }
 }
 
-// Loops playRound 5 times to play a full game of rock paper scissors
-function game() {
-  // Loop to play through 5 games
-  for (i = 1; i < 4; i++) {
-    roundNumber++;
-    // Play a round of Rock Paper Scissors
-    playRound();
+function showWinner(winner, computerChoice) {
+  if (winner === 'player') {
+    // Inc player score
+    scoreboard.player++;
+    // Show modal result
+    result.innerHTML =
+      `
+      <h1 class="text-win">You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  } else if (winner === 'computer') {
+    // Inc computer score
+    scoreboard.computer++;
+    // Show modal result
+    result.innerHTML =
+      `
+      <h1 class="text-lose">You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  } else {
+    result.innerHTML =
+      `
+      <h1>It's A Draw</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  }
+  // Show score
+  score.innerHTML =
+    `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Computer: ${scoreboard.computer}</p>
+    `;
 
-    console.log(roundResult);
+  modal.style.display = 'block';
+}
 
-    // Final score check
-    if (i === 3) {
-      if (playerScore > computerScore) {
-        alert(
-          "Congratulations! You won " +
-            playerScore +
-            " to " +
-            computerScore +
-            "."
-        );
-        return;
-      } else if (playerScore < computerScore) {
-        alert(
-          "Sorry, it looks like you lost " +
-            playerScore +
-            " to " +
-            computerScore +
-            "."
-        );
-        return;
-      } else {
-        alert(
-          "Looks like it was a draw! " +
-            playerScore +
-            " to " +
-            computerScore +
-            "."
-        );
-        return;
-      }
-    }
+// Restart game
+function restartGame() {
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  score.innerHTML = `
+    <p>Player: 0</p>
+    <p>Computer: 0</p>
+  `;
+}
+
+// Clear modal
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
   }
 }
 
-game();
+// Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
